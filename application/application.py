70f1e242685c9afabbbedbeeadf3709e999d1716
Tim Huegdon <tim@nefariousdesigns.co.uk>
2009-03-26 08:19:37 +0000

    grab cache.

--- application/application.py
+++ application/application.py
@@ -7,20 +7,28 @@ TEMPLATES_BASE  = os.path.join( APP_BASE, 'templates' )
 
 sys.path.insert( 0, LIB_BASE )
 
+import pickle
 from newf import Application, Response, ResponseRedirect
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment( loader = FileSystemLoader( TEMPLATES_BASE ) )
 
 def root(request):
-    context = {
+	cache = open( os.path.join( APP_BASE, 'var/cache/cache.pkl' ), 'rb')
+
+	tweets = pickle.load(cache)
+	
+	cache.close()
+	
+	context = {
         'body' : {
             'id' : 'index'
         },
+		'tweets' : tweets
     }
-    
-    template = env.get_template('master.html')
-    return Response(template.render(context))
+
+	template = env.get_template('master.html')
+	return Response(template.render(context))
 
 urls = (
 	(r'^/$', root),
